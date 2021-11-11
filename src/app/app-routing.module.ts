b86168b96ac60787b0bab1109c611e9d8d42c8bf
EdwinZ01 <helloteam.developer7@gmail.com>
2021-11-11 10:57:06 -0600

    make it better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApiComponent } from './View/api/api.component';
+import { EjemploComponent } from './View/ejemplo/ejemplo.component';
+import { InicioComponent } from './View/inicio/inicio.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'api', component: ApiComponent},
+  {path: 'ejemplo', component: EjemploComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ColorPickerModule } from '@syncfusion/ej2-angular-inputs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './View/inicio/inicio.component';
+import { MenuComponent } from './View/menu/menu.component';
+import { ApiComponent } from './View/api/api.component';
+import { EjemploComponent } from './View/ejemplo/ejemplo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    MenuComponent,
+    ApiComponent,
+    EjemploComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
