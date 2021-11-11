0c4e22ba8cbd85177f73d15c52bbd57a55690c1d
EdwinZ01 <helloteam.developer7@gmail.com>
2021-11-11 11:38:46 -0600

    make it better

--- src/app/View/ejemplo/ejemplo.component.ts
+++ src/app/View/ejemplo/ejemplo.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ejemplo',
   templateUrl: './ejemplo.component.html',
-  styleUrls: ['./ejemplo.component.css']
+  
 })
 export class EjemploComponent implements OnInit {
 
